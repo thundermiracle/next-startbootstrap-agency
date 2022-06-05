94de1f5b9cbc14ae78f2213c622c1b7eab4d5331
thundermiracle <cissoliu@gmail.com>
2022-06-05 21:46:06 +0900

    feat: move to src folder & add unocss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,8 @@
-import '../styles/globals.css';
+import '@unocss/reset/normalize.css';
+import '@unocss/reset/sanitize/sanitize.css';
+import '@unocss/reset/sanitize/assets.css';
+import '../styles/uno.css';
+
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
