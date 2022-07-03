2c5ebd24f926513f53812b3e9f1a52bdf166c356
thundermiracle <cissoliu@gmail.com>
2022-07-03 22:24:19 +0900

    feat: fix style of timeline in sp mode

--- src/components/organisms/PageSection.tsx
+++ src/components/organisms/PageSection.tsx
@@ -13,7 +13,10 @@ interface Props {
 
 const PageSection: FC<Props> = ({ id, className, title, subTitle, footer, children }) => {
   return (
-    <section id={id} className={clsx('py-150px md:py-100px text-center', className)}>
+    <section
+      id={id}
+      className={clsx('py-150px md:py-100px text-center md:px-0 px-12px', className)}
+    >
       {title && <h2 className="case-upper text-40px mt-0 mb-15px">{title}</h2>}
       {subTitle && (
         <h3 className="text-muted text-16px font-400 mb-75px mt-0 font-italic">{subTitle}</h3>
