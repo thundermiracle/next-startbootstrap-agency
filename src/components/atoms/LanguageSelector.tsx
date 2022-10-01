7090bcb24f8ff69a0e7e8007c1d90abe0a364859
ThunderMiracle <cissoliu@gmail.com>
2022-10-01 11:34:58 +0900

    feat: apply i18n with japanese support (#51)

--- src/components/atoms/LanguageSelector.tsx
+++ src/components/atoms/LanguageSelector.tsx
@@ -7,43 +7,37 @@ import IconText from './IconText';
 import type { FC } from 'react';
 
 interface Props {
-  defaultLang?: string;
   langKey?: string;
   langTextMap?: Record<string, string>;
 }
 
 const LanguageSelector: FC<Props> = ({
-  defaultLang = 'en',
   langKey = 'en',
   langTextMap = {
     en: 'English',
   },
 }) => {
   return (
-    <NavDropdown
-      title={<IconText iconName="i-fa-globe" text={langTextMap[langKey]} />}
-      id="language-dropdown"
-      className="color-white hover:color-primary"
-      color="primary"
-    >
-      {Object.keys(langTextMap).map((key) => {
-        return (
-          <Link key={key} href={getBaseUrl(defaultLang, key)}>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-            <a className={clsx('dropdown-item', { active: key === langKey })}>{langTextMap[key]}</a>
-          </Link>
-        );
-      })}
-    </NavDropdown>
+    <>
+      <NavDropdown
+        title={<IconText iconName="i-fa-globe" text={langTextMap[langKey]} />}
+        id="language-dropdown"
+        className="color-white hover:color-primary mt-5px md:mt-0 md:ml-12px"
+        color="primary"
+      >
+        {Object.keys(langTextMap).map((key) => {
+          return (
+            <Link key={key} href="/" locale={key}>
+              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+              <a className={clsx('dropdown-item', { active: key === langKey })}>
+                {langTextMap[key]}
+              </a>
+            </Link>
+          );
+        })}
+      </NavDropdown>
+    </>
   );
 };
 
 export default LanguageSelector;
-
-function getBaseUrl(defaultLang: string, lang: string) {
-  if (defaultLang !== lang) {
-    return `/${lang}/`;
-  }
-
-  return '/';
-}
