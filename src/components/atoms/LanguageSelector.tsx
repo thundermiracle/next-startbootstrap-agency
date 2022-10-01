8ecc4709fffd66ac3317f4644814a77b193e5d77
thundermiracle <cissoliu@gmail.com>
2022-10-01 16:23:18 +0900

    fix: automatically close language dropdown after selecting

--- src/components/atoms/LanguageSelector.tsx
+++ src/components/atoms/LanguageSelector.tsx
@@ -25,13 +25,15 @@ const LanguageSelector: FC<Props> = ({
         className="color-white hover:color-primary mt-5px md:mt-0 md:ml-12px"
         color="primary"
       >
-        {Object.keys(langTextMap).map((key) => {
+        {Object.keys(langTextMap).map((locale) => {
           return (
-            <Link key={key} href="/" locale={key}>
-              {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-              <a className={clsx('dropdown-item', { active: key === langKey })}>
-                {langTextMap[key]}
-              </a>
+            <Link key={locale} href="/" locale={locale}>
+              <NavDropdown.Item
+                href={locale}
+                className={clsx('dropdown-item', { active: locale === langKey })}
+              >
+                {langTextMap[locale]}
+              </NavDropdown.Item>
             </Link>
           );
         })}
