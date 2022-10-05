ded7d7cb82a2249189cfa7f03010e2b43aa8433d
ThunderMiracle <cissoliu@gmail.com>
2022-10-05 22:20:37 +0900

    feat: add pwa (#58)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const runtimeCaching = require('next-pwa/cache');
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  runtimeCaching,
+});
+
+const nextConfig = withPWA({
   reactStrictMode: true,
   i18n: {
     locales: ['en', 'ja'],
     defaultLocale: 'en',
   },
-};
+});
 
 module.exports = nextConfig;
