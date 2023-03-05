70f1abef452cc3266656447ea1b89fcb6cd62557
ThunderMiracle <cissoliu@gmail.com>
2023-03-05 11:40:40 +0900

    test: automatically remove vercel-live-comment dom in test

--- e2e/visual.spec.ts
+++ e2e/visual.spec.ts
@@ -1,6 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('example test', async ({ page }) => {
+test('visual landing page test', async ({ page }) => {
   await page.goto('');
+
+  try {
+    const vercelPreviewComments = await page.waitForSelector('vercel-live-feedback', {
+      timeout: 1000,
+    });
+    await vercelPreviewComments.evaluate((node) => node.remove());
+  } catch {
+    // eslint-disable-next-line no-console
+    console.log("vercel preview comments doesn't exist");
+  }
+
   await expect(page).toHaveScreenshot({ fullPage: true });
 });
