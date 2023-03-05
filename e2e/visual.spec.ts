1123fbe51f9e34f38a53161036d2fda7245edabc
ThunderMiracle <cissoliu@gmail.com>
2023-03-05 12:57:02 +0900

    test: remove vercel live comment part

--- e2e/visual.spec.ts
+++ e2e/visual.spec.ts
@@ -3,14 +3,10 @@ import { test, expect } from '@playwright/test';
 test('visual landing page test', async ({ page }) => {
   await page.goto('');
 
-  try {
-    const vercelPreviewComments = await page.waitForSelector('vercel-live-feedback', {
-      timeout: 1000,
-    });
+  // remove vercel live comments part
+  const vercelPreviewComments = await page.$('vercel-live-feedback');
+  if (vercelPreviewComments) {
     await vercelPreviewComments.evaluate((node) => node.remove());
-  } catch {
-    // eslint-disable-next-line no-console
-    console.log("vercel preview comments doesn't exist");
   }
 
   await expect(page).toHaveScreenshot({ fullPage: true });
